4f04229e4d9414e367f5a7766b3e7815b36a9d06
Farrukh <ffarrux386@gmail.com>
2026-05-05 21:29:57 +0500

    Initial setup

---
+++ src/main/java/com/farrukh/aireview/AiCodeReviewAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.farrukh.aireview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeReviewAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCodeReviewAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/farrukh/aireview/AiCodeReviewAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farrukh.aireview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeReviewAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
